3c7c147d09c3b4502fdbb988df44bc19710ebcc9
Aleksander <mawr1k66@gmail.com>
2019-01-28 17:42:27 +0200

    about

--- assets/js/common.js
+++ assets/js/common.js
@@ -43,7 +43,7 @@ $(function () {
 
     $(window).on('mousewheel', function(e){
         if (e.originalEvent.wheelDelta < 0) {
-            if ($(window).scrollTop() < $('.first-block').height()-300) {
+            if ($(window).scrollTop() < $('.first-block').height()-500) {
                 $('html, body').animate({
                     scrollTop: $('.wwd').offset().top-$('header').height()
                 });
@@ -59,5 +59,10 @@ $(function () {
             $('.mymodal-container').removeClass('open');
         }
     });
+
+    $('.up-btn').click(function() {
+        $('html, body').animate({scrollTop: 0},500);
+        return false;
+    })
 
 });
